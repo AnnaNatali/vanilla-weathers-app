ee943d401a7a8a09a618791b05a7334d954fdbb2
AnnaNatali <132666924+AnnaNatali@users.noreply.github.com>
2023-09-01 00:56:31 +0200

    added 7 days forecast in js

--- src/app.js
+++ src/app.js
@@ -23,8 +23,12 @@ function formatDate(timestamp) {
 
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = `
-           <div class="row">
+  let forecastHTML = `<div class="row">`;
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
               <div class="col-2">
                 <div class="weather-forecast-date">Mon</div>
                 <img
@@ -36,8 +40,10 @@ function displayForecast() {
                   <span class="weather-forecast-temperature-max">18℃</span>
                   <span class="weather-forecast-temperature-min">12℃</span>
                 </div>
-              </div>
             </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
